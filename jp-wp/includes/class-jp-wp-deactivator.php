45563f92a5b502e8b8ff85014a38b9274c16f057
Hang Sun <hsun@connexity.com>
2021-07-16 10:40:55 -0700

    Init commit

---
+++ jp-wp/includes/class-jp-wp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    JP_WP
+ * @subpackage JP_WP/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    JP_WP
+ * @subpackage JP_WP/includes
+ * @author     Your Name <email@example.com>
+ */
+class JP_WP_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ jp-wp/includes/class-jp-wp-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    JP_WP
+ * @subpackage JP_WP/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    JP_WP
+ * @subpackage JP_WP/includes
+ * @author     Your Name <email@example.com>
+ */
+class JP_WP_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'jp-wp',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
